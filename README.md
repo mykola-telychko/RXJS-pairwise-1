2b4711f76f213065d061a0d262ee62c6260f71f4
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-10 07:13:15 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-pairwise-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-kyfwpg)
---
+++ index.ts
@@ -0,0 +1,7 @@
+import { pairwise, take } from 'rxjs/operators';
+import { interval } from 'rxjs';
+
+//Returns: [0,1], [1,2], [2,3], [3,4], [4,5]
+interval(1000)
+  .pipe(pairwise('0'), take(5))
+  .subscribe(cons]ole.log);
